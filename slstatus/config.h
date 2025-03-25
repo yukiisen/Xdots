cc64cc07e200fa24bae4b7d27dc53ab86075bc3d
yukiisen <ahmad.redhorn@gmail.com>
2025-03-25 23:38:41 +0100

    Cleaning stuff!

--- slstatus/config.h
+++ slstatus/config.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
     /* function format          argument */
-    {disk_perc, "   %s% |", "/home"},
+    {disk_perc, "   %s% |", "/"},
     {cpu_perc, "   %s% |", NULL},
     {keymap, "   %s |", NULL},
     {ram_used, "   %s |", NULL},
--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static const char *font = "Fira Code Nerd Font Propo:size=10:style=Bold";
+static const char *font = "Jetbrains Mono:size=10:style=Bold";
 /* Spare fonts */
 static char *font2[] = {
     "Inconsolata for Powerline:pixelsize=11:antialias=true:autohint=true"};
