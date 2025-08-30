37ba433b91dfab783f690484d11e64f17de66670
yukiisen <ahmad.redhorn@gmail.com>
2025-08-30 20:49:56 +0100

    العاعاعوط

--- fetch.fish
+++ fetch.fish
@@ -6,6 +6,7 @@ cp ~/.config/picom/ ./ -r
 cp ~/.config/foot/ ./ -r 
 cp ~/.config/sway/ ./ -r
 cp ~/.config/nvim/ ./ -r
+cp ~/.config/yambar/ ./ -r
 cp ~/.scripts/* ./scripts/
 cp ~/.vimrc ./
 cp ~/.tmux.conf ./
